3b0b382f54e26c0e590657c2bb4bda6699c805ff
windx-foobar <bezalkogoln1ycoder@gmail.com>
2021-06-28 22:03:56 +0300

    Добавлен phpstan в travis и проект

--- src/Hello.php
+++ src/Hello.php
@@ -6,9 +6,23 @@ namespace WindxFoobar\PhpComposerPackage;
 
 /**
  * Class Hello
+ *
+ * @property-read string $test
+ *
  * @package WindxFoobar\PhpComposerPackage
  */
 class Hello {
+   private string $test;
+
    public function __construct() {
+      echo 'hello world';
+   }
+
+   public function __get(string $name): string {
+      return $this->$name;
+   }
+
+   public function main(): string {
+      return $this->test;
    }
 }
